1ce2783772ad91585cd959d5880151e498b2c94c
Szymon Pyżalski <szymon@pythonista.net>
2011-12-16 07:11:48 +0100

    All cases of domain/port syntax errors tested

---
+++ src/urlmap/test/test_exc/test_domain_twice.py
@@ -0,0 +1,21 @@
+import os
+import sys
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from paste.deploy import loadapp
+from urlmap.test.util import make_app
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(HERE)
+
+class Test(unittest.TestCase):
+    """Throw error when path specified as 'domain'"""
+
+    def _broken(self):
+        return loadapp('config:test_domain_twice.ini', relative_to=HERE)
+
+    def test_domain_twice(self):
+        self.assertRaises(ValueError, self._broken)
